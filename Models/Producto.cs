ebc90ef7f41b8e7ab3a06dd378def07096e4c8a1
luisnrq <luisenrique.frias@tajamar365.com>
2022-05-06 10:56:31 +0200

    commit

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -10,6 +10,7 @@ namespace MvcAWSCacheRedis.Models
         public int IdProducto { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        public int Precio { get; set; }
         public string Imagen { get; set; }
     }
 }
