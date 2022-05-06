c745710f6a99361dee9869a85948dde355274a1d
luisnrq <luisenrique.frias@tajamar365.com>
2022-05-06 13:04:56 +0200

    commit

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcAWSCacheRedis.Models;
 using MvcAWSCacheRedis.Repositories;
+using MvcAWSCacheRedis.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,12 @@ namespace MvcAWSCacheRedis.Controllers
     public class ProductosController : Controller
     {
         private RepositoryProductos repo;
+        private ServiceCacheAWS service;
 
-        public ProductosController(RepositoryProductos repo)
+        public ProductosController(RepositoryProductos repo, ServiceCacheAWS service)
         {
             this.repo = repo;
+            this.service = service;
         }
 
         public IActionResult Index()
@@ -28,5 +31,31 @@ namespace MvcAWSCacheRedis.Controllers
             Producto producto = this.repo.FindProducto(id);
             return View(producto);
         }
+
+        public IActionResult SeleccionarFavorito(int id)
+        {
+            //Buscamos el producto a almacenar dentro del repo
+            Producto producto = this.repo.FindProducto(id);
+
+            //almacenamos el producto dentro de cache
+            this.service.AddProductoCache(producto);
+
+            TempData["MENSAJE"] = "Producto " + producto.Nombre + " almacenado como Favorito";
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Favoritos()
+        {
+            List<Producto> productos = this.service.GetProductosCache();
+            return View(productos);
+        }
+
+        public IActionResult EliminarFavorito(int id)
+        {
+            this.service.EliminarProductoCache(id);
+            return RedirectToAction("Favoritos");
+        }
+
     }
 }
